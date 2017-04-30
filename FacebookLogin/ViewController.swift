81281f2b8c1a888ef9fab7d09dcc3ae29ad5b52c
Marcelly Luise <marcellygodinho@gmail.com>
2017-04-30 13:57:53 -0300

    Initial commit: an working example of facebook login with Facebook Button - pod file setup: adding facebook SDK - plist: updating plist with facebook required keys - App Delegate: setting up delegates - Controller: Adding facebook button and checking facebook access token

--- FacebookLogin/ViewController.swift
+++ FacebookLogin/ViewController.swift
@@ -7,19 +7,56 @@
 //
 
 import UIKit
+import FBSDKCoreKit
+import FBSDKLoginKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        if FBSDKAccessToken.current() == nil {
+            
+        } else {
+            print(FBSDKAccessToken.current().appID)
+        }
+        
+        setupFacebookButton()
+        
     }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    
+    // MARK: - Facebook Login
+    func setupFacebookButton() {
+        let facebookLoginButton = FBSDKLoginButton()
+        
+        facebookLoginButton.center = view.center
+        facebookLoginButton.delegate = self
+        
+        view.addSubview(facebookLoginButton)
+    }
 
 }
 
+extension ViewController: FBSDKLoginButtonDelegate {
+    func loginButton(_ loginButton: FBSDKLoginButton!, didCompleteWith result: FBSDKLoginManagerLoginResult!, error: Error!) {
+        print(#function)
+        print(result)
+    }
+    
+    func loginButtonWillLogin(_ loginButton: FBSDKLoginButton!) -> Bool {
+        print(#function)
+        return true
+    }
+    
+    func loginButtonDidLogOut(_ loginButton: FBSDKLoginButton!) {
+        print(#function)
+    }
+}
---
+++ Podfile
@@ -0,0 +1,12 @@
+# Uncomment this line to define a global platform for your project
+platform :ios, '9.3'
+# Uncomment this line if you're using Swift
+use_frameworks!
+inhibit_all_warnings!
+
+target ‘FacebookLogin’ do
+    pod 'FacebookCore'
+    pod 'FacebookLogin'
+    pod 'FacebookShare'
+
+end
